06e22762b42ca6d19cdc3315937814f26a54d1d6
pinalef <tierralavista@gmail.com>
2017-11-12 20:30:33 -0300

    Versión 0.0.1

--- app.js
+++ app.js
@@ -1,31 +1,26 @@
 function tareaPrincipal(){
-
     //obtiene div lista
    var lista = document.getElementById('lista');
   // parent node
   var parentDiv = lista.parentNode;
-  console.log(parentDiv);
 
    //creo contenedor formulario, y elementos
    var contLista = document.createElement('div');
    var input = document.createElement('input');
    var button = document.createElement('input');
 
-   console.log(contLista);
-
    //Agrego elementos a contenedor
    contLista.appendChild(input);
    contLista.appendChild(button);
  
-  
- 
    //agrego style
    contLista.setAttribute('id', 'contlista');
    input.setAttribute('type', 'text');
-   input.setAttribute('text', 'Anadir una lista...');
+   input.value = 'Añadir una lista ...';
    button.setAttribute('type', 'button');
-   console.log(button);
+   button.value = 'Agregar';
 
    //reemplazo nodo
     parentDiv.replaceChild(contLista, lista);
+
 }
