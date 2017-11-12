1e32839e210a1cdbcc52332b26f7f1bc8ad56393
pinalef <tierralavista@gmail.com>
2017-11-12 20:57:56 -0300

    Versión 0.0.2

--- app.js
+++ app.js
@@ -23,4 +23,30 @@ function tareaPrincipal(){
    //reemplazo nodo
     parentDiv.replaceChild(contLista, lista);
 
+    //anado evento al input texto
+    input.addEventListener('click', function(){
+    input.value = "";
+    agregar();
+    })
+
+    function agregar(){
+        button.addEventListener('click', function(){
+            var texto = input.value;
+            input.value = "";
+            //creo nuevos elementos, parrafo y link
+            var parrafo = document.createElement('p');
+            var enlace = document.createElement('a');
+            var parrafo1 = document.createTextNode(texto);
+            var enlace1 = document.createTextNode('Añadir una tarea');
+
+            //anado contenido a parrafo y link
+            parrafo.appendChild(parrafo1);
+            enlace.appendChild(enlace1);
+
+            //reemplazo nodos             
+            contLista.replaceChild(parrafo, input);
+            contLista.replaceChild(enlace, button);
+        })
+    }
 }
+
